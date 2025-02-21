5cfcada26af5fdeeebf9acf21045724c48ab72b4
ANGELI WICKRAMA ARACHCHI <159871522+angeli-sliit@users.noreply.github.com>
2025-02-21 15:55:14 +0530

    Update Footer.js email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/angeli-wickrama-arachchige-649502293" target="_blank" rel="noopener noreferrer">
             <i className="bx bxl-linkedin"></i>
           </a>
-            <a href="mailto:angeli.2003it@gmail.com">
+            <a href="mailto:it23144408@my.sliit.lk">
             <i className="bx bx-envelope"></i>
           </a>
         </div>
